98d3b05db7bc1ecc480ecf33eade656b06aadd19
Abdelrahman Magdy EL Monshed <amgo123@gmail.com>
2023-05-01 23:21:28 +0200

    fix issues

--- resources/views/backend/post_categories/create.blade.php
+++ resources/views/backend/post_categories/create.blade.php
@@ -80,16 +80,45 @@
 <script src="{{ asset('assets/backend/plugins/custom/ckeditor/ckeditor-balloon.bundle.js') }}"></script>
 <script src="{{ asset('assets/backend/plugins/custom/ckeditor/ckeditor-balloon-block.bundle.js') }}"></script>
 <script src="{{ asset('assets/backend/plugins/custom/ckeditor/ckeditor-document.bundle.js') }}"></script>
-<script>
-   .create(document.querySelector('#kt_docs_ckeditor_classic'))
-   .then(editor => {
-       console.log(editor);
-   })
-   .catch(error => {
-       console.error(error);
-   });
-</script>
-
 @include('backend.form')
 <!--end::Custom Javascript-->
+<script>
+
+
+// Class definition
+var KTQuilDemos = function() {
+
+// Private functions
+var demo1 = function() {
+    var quill = new Quill('#kt_quil_1', {
+        modules: {
+            toolbar: [
+                [{
+                    header: [1, 2, false]
+                }],
+                ['bold', 'italic', 'underline'],
+                ['image', 'code-block']
+            ]
+        },
+        placeholder: 'Type your text here...',
+        theme: 'snow' // or 'bubble'
+    });
+}
+
+return {
+    // public functions
+    init: function() {
+        demo1();
+        demo2();
+    }
+};
+}();
+
+jQuery(document).ready(function() {
+KTQuilDemos.init();
+});
+
+
+</script>
+   
 @stop
--- resources/views/components/backend/cms/description.blade.php
+++ resources/views/components/backend/cms/description.blade.php
@@ -1,32 +1,15 @@
-<div>
-	<!--begin::Label-->
-	<label class="form-label">Description [{{ $lang }}] </label>
-	<!--end::Label-->
-	<!--begin::Editor-->
 
 
-	<textarea name="kt_docs_ckeditor_classic" id="kt_docs_ckeditor_classic">
-		<h1>Quick and simple CKEditor 5 Integration</h1>
-		<p>Here goes the <a href="#">Minitial content of the editor</a>. Lorem Ipsum is simply dummy text of the <em>printing and typesetting</em> industry.</p>
-		<blockquote>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</blockquote>
-		<ul>
-			<li>List item 1</li>
-			<li>List item 2</li>
-			<li>List item 3</li>
-			<li>List item 4</li>
-		</ul>
-		<figure class="image"><img src="assets/media/stock/600x400/img-1.jpg" alt="CKEditor Demo"/></figure>
-		Here goes the <a href="#">Minitial content of the editor</a>. Lorem Ipsum is simply dummy text of the <em>printing and typesetting</em> industry.
-		<h1>Easy Media Embeds</h1>
-		<figure class="symbol">
-			<oembed url="https://www.youtube.com/watch?v=d-pSVf8Xazk"></oembed>
-		</figure>
-		<p>Lorem ipsum dolor sit amet,consectetuer edipiscing elit,sed diam nonummy nibh euismod tinciduntut laoreet doloremagna aliquam erat volutpat.Ut wisi enim ad minim veniam,quis nostrud exerci tation ullamcorper. Lorem ipsum dolor sit amet,consectetuer edipiscing elit,sed diam nonummy nibh euismod tinciduntut laoreet doloremagna aliquam erat volutpat.Ut wisi enim ad minim veniam,quis nostrud exerci tation ullamcorper.</p>
-	</textarea>
-	
-	
-	{{-- <div id="kt_ecommerce_add_product_description" name="kt_ecommerce_add_product_description" class="min-h-200px mb-2"></div> --}}
-	<!--end::Editor-->
-	<!--begin::Description-->
-	<!--end::Description-->
+<div class="card card-custom">
+    <div class="card-header">
+        <h3 class="card-title">
+            Basic Demo
+        </h3>
+    </div>
+    <div class="card-body">
+        <div id="kt_quil_1" style="height: 325px">
+            Compose a message
+        </div>
+    </div>
 </div>
+

68af2b5c463f5c4cfc57614bde811deb48e3c6ee
Abdelrahman Magdy EL Monshed <amgo123@gmail.com>
2023-05-15 09:24:49 +0300

    clear chaing cron job every week

--- resources/views/backend/post_categories/create.blade.php
+++ resources/views/backend/post_categories/create.blade.php
@@ -67,32 +67,13 @@ color: #f1416c;
         <?php $lang = substr($properties2['regional'],0,2);?>
         <div class="tab-pane fade {{ LaravelLocalization::getCurrentLocaleName() == $properties2['name'] ? 'show active':''}}" id="{{ substr($properties2['regional'],0,2) }}" role="tabpanel">
 
-
-            
-            <div class="form-group row">
-                <div class="fl w-100">
-                    <div class="fl w-25 pa2">UN{{ $lang }}</div>
-                    <div class="fl w-40">
-                        <input
-                            type="text"
-                            class="input-reset ba b--black-20 pa2 mb2 db w-100"
-                            name="username_{{ $lang }}"
-                            id="username_{{ $lang }}"
-                            required
-                            data-fv-not-empty___message="The username is required"
-                            pattern="^[a-zA-Z0-9]+$"
-                            data-fv-regexp___message="The username can only consist of alphabetical, number"
-                        />
-                    </div>
-                </div>
-            </div>
+      
             
             <div class="form-group row">
                 <label class="col-lg-12 control-label" for="title_{{ $lang }}">  [{{ $lang }}] <span class="text-danger">*</span></label>
                 <div class="col-lg-12">
                 
                  
-
                     <input type="text"
                     id="title_{{ $lang }}" name="title_{{ $lang }}"
                     type="text"
@@ -100,8 +81,7 @@ color: #f1416c;
                     required
                     data-fv-not-empty___message="title {{ $lang }} is required"
                     />
-
-                    
+                
                 </div>
                 </div>
                 
@@ -140,8 +120,10 @@ color: #f1416c;
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.3/js/bootstrapValidator.min.js" integrity="sha256-PbFF1Mdg86urwOYXWNJPP4z5Ge9KLp6KXX1NURQY8Ho=" crossorigin="anonymous"></script>
 <script src="https://www.chineseshaolins.com/js/formvalidation/plugins/Tachyons.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.3/es6-shim.min.js"></script>
+<script src="{{ asset('assets/backend/plugins/custom/ckeditor/ckeditor-classic.bundle.js') }}"></script>
 <script src="{{ asset('assets/backend/plugins/custom/datatables/datatables.bundle.js')}}"></script>
  
+<script src="{{ asset('assets/backend/abdo/apply2.js')}}"></script>
  
 <script type="text/javascript">
 
@@ -149,18 +131,12 @@ color: #f1416c;
 
     //  End Ajax country and city 
  
-var validator;
-
-var form = document.getElementById('kt_ecommerce_add_category_form');
-const submitButton = document.getElementById('kt_ecommerce_add_category_submit');
-
-
-
-////
-document.addEventListener('DOMContentLoaded', function (e) {
-                FormValidation.formValidation(document.getElementById('kt_ecommerce_add_category_form'), {
-                    plugins: {
-                        declarative: new FormValidation.plugins.Declarative({
+$(document).ready(function() {
+$('#kt_ecommerce_add_category_form')
+    .bootstrapValidator({
+        plugins: {
+      
+            declarative: new FormValidation.plugins.Declarative({
                             html5Input: true,
                         }),
                         trigger: new FormValidation.plugins.Trigger(),
@@ -171,55 +147,11 @@ document.addEventListener('DOMContentLoaded', function (e) {
                             invalid: 'fa fa-times',
                             validating: 'fa fa-refresh',
                         }),
-                    },
-                })    .on('status.field.bv', function(e, data) {
-        var $form     = $(e.target),
-            validator = data.bv,
-            $tabPane  = data.element.parents('.tab-pane'),
-            tabId     = $tabPane.attr('id');
-        
-        if (tabId) {
-            var $icon = $('a[href="#' + tabId + '"][data-toggle="tab"]').parent().find('i');
 
-            // Add custom class to tab containing the field
-            if (data.status == validator.STATUS_INVALID) {
-                $icon.removeClass('fa-check').addClass('fa-times');
-            } else if (data.status == validator.STATUS_VALID) {
-                var isValidTab = validator.isValidContainer($tabPane);
-                $icon.removeClass('fa-check fa-times')
-                     .addClass(isValidTab ? 'fa-check' : 'fa-times');
-            }
-        }
-    });
-;
-            });
-////
-
-    $(document).ready(function() {
-$('#kt_ecommerce_add_category_formX').bootstrapValidator({
-       
+          
+      }
  
-    framework: 'bootstrap',
-            excluded: [':disabled'],
-
-                
-                plugins: {
-                    declarative: new FormValidation.plugins.Declarative({
-                    html5Input: true,
-                    }),
-                    trigger: new FormValidation.plugins.Trigger(),
-                    tachyons: new FormValidation.plugins.Tachyons({
-                        rowInvalidClass: 'my-field-error',
-                        rowValidClass: 'my-field-success',
-                    }),
-                    submitButton: new FormValidation.plugins.SubmitButton(),
-                    icon: new FormValidation.plugins.Icon({
-                        valid: 'fa fa-check',
-                        invalid: 'fa fa-times',
-                        validating: 'fa fa-refresh',
-                    }),
-                }
-
+ 
     })
     .on('status.field.bv', function(e, data) {
         var $form     = $(e.target),

fd40990eb78d3a7866c369d63748d234480c5825
Abdelrahman Magdy EL Monshed <amgo123@gmail.com>
2023-05-21 16:57:45 +0300

    code

--- resources/views/backend/post_categories/create.blade.php
+++ resources/views/backend/post_categories/create.blade.php
@@ -52,60 +52,201 @@ top: 30px;
  
   <div class="container-xxl" id="kt_content_container">
   
+    <form id="kt_ecommerce_add_product_form" class="form d-flex flex-column flex-lg-row" data-kt-redirect="../../demo7/dist/apps/ecommerce/catalog/products.html">
+        <!--begin::Aside column-->
  
- 
-<form id="kt_ecommerce_add_category_form" action="get" class="form d-flex flex-column flex-lg-row" data-kt-redirect="#">
-    <!--begin::Aside column-->
- 
-    <!--end::Aside column-->
-    <!--begin::Main column-->
-    <div class="d-flex flex-column flex-row-fluid gap-7 gap-lg-10">
+        <!--end::Aside column-->
+        <!--begin::Main column-->
+        <div class="d-flex flex-column flex-row-fluid gap-7 gap-lg-10">
+            <!--begin:::Tabs-->
 
-        <div class="card card-flush py-4">
-            <div class="card-body pt-0">              
-             
-                
-                <div class="mb-10 fl row">
-                    <!--begin::Label-->
-                    <label class="required form-label">Product Name</label>
-                    <!--end::Label-->
-                    <!--begin::Input-->
-                    
-                    
-                    <input
-                                class="form-control mb-2 ba b--black-20 pa2 mb2 db w-100"
-                                name="website"
-                                type="text"
-                               
-                                required
-                                data-fv-not-empty___message="The website is required"
-                            />
 
-                    <!--end::Input-->
-                    <!--begin::Description-->
-                    <div class="text-muted fs-7">A product name is required and recommended to be unique.</div>
-                    <!--end::Description-->
-                </div>
-
-                
             
+
+
+
+ 
+            
+            
+
+
+
+
+            <ul class="nav nav-custom nav-tabs nav-line-tabs nav-line-tabs-2x border-0 fs-4 fw-semibold mb-n2">
+                <!--begin:::Tab item-->
+                <li class="nav-item">
+                    <a class="nav-link text-active-primary pb-4 active" data-bs-toggle="tab" href="#kt_ecommerce_add_product_general">General</a>
+                </li>
+                <!--end:::Tab item-->
+                <!--begin:::Tab item-->
+                <li class="nav-item">
+                    <a class="nav-link text-active-primary pb-4" data-bs-toggle="tab" href="#kt_ecommerce_add_product_advanced">Advanced</a>
+                </li>
+                <!--end:::Tab item-->
+            </ul>
+            <!--end:::Tabs-->
+            <!--begin::Tab content-->
+            <div class="tab-content">
+                <!--begin::Tab pane-->
+                <div class="tab-pane fade show active" id="kt_ecommerce_add_product_general" role="tab-panel">
+                    <div class="d-flex flex-column gap-7 gap-lg-10">
+                        <!--begin::General options-->
+                        <div class="card card-flush py-4">
+                            <!--begin::Card header-->
+                            <div class="card-header">
+                                <div class="card-title">
+                                    <h2>General</h2>
+                                </div>
+                            </div>
+                            <!--end::Card header-->
+                            <!--begin::Card body-->
+                            <div class="card-body pt-0">
+                                <!--begin::Input group-->
+                                <div class="mb-10 fv-row">
+                                    <!--begin::Label-->
+                                    <label class="required form-label">Product Name</label>
+                                    <!--end::Label-->
+                                    <!--begin::Input-->
+                                    <input type="text" name="product_name" class="form-control mb-2" placeholder="Product name" value="" />
+                                    <!--end::Input-->
+                                    <!--begin::Description-->
+                                    <div class="text-muted fs-7">A product name is required and recommended to be unique.</div>
+                                    <!--end::Description-->
+                                </div>
+                                <!--end::Input group-->
+                                <!--begin::Input group-->
+                                <div>
+                                    <!--begin::Label-->
+                                    <label class="form-label">Description</label>
+                                    <!--end::Label-->
+                                    <!--begin::Editor-->
+                                    <div id="kt_ecommerce_add_product_description" name="kt_ecommerce_add_product_description" class="min-h-200px mb-2"></div>
+                                    <!--end::Editor-->
+                                    <!--begin::Description-->
+                                    <div class="text-muted fs-7">Set a description to the product for better visibility.</div>
+                                    <!--end::Description-->
+                                </div>
+                                <!--end::Input group-->
+                            </div>
+                            <!--end::Card header-->
+                        </div>
+                        <!--end::General options-->
+                        <!--begin::Media-->
+                         
+                        <!--end::Media-->
+                        <!--begin::Pricing-->
+                       
+                        <!--end::Pricing-->
+                    </div>
+                </div>
+                <!--end::Tab pane-->
+                <!--begin::Tab pane-->
+                <div class="tab-pane fade" id="kt_ecommerce_add_product_advanced" role="tab-panel">
+                    <div class="d-flex flex-column gap-7 gap-lg-10">
+                        <!--begin::Inventory-->
+                        <div class="card card-flush py-4">
+                            <!--begin::Card header-->
+                            <div class="card-header">
+                                <div class="card-title">
+                                    <h2>Inventory</h2>
+                                </div>
+                            </div>
+                            <!--end::Card header-->
+                            <!--begin::Card body-->
+                            <div class="card-body pt-0">
+                                <!--begin::Input group-->
+                                <div class="mb-10 fv-row">
+                                    <!--begin::Label-->
+                                    <label class="required form-label">SKU</label>
+                                    <!--end::Label-->
+                                    <!--begin::Input-->
+                                    <input type="text" name="sku" class="form-control mb-2" placeholder="SKU Number" value="" />
+                                    <!--end::Input-->
+                                    <!--begin::Description-->
+                                    <div class="text-muted fs-7">Enter the product SKU.</div>
+                                    <!--end::Description-->
+                                </div>
+                                <!--end::Input group-->
+                                <!--begin::Input group-->
+                                <div class="mb-10 fv-row">
+                                    <!--begin::Label-->
+                                    <label class="required form-label">Barcode</label>
+                                    <!--end::Label-->
+                                    <!--begin::Input-->
+                                    <input type="text" name="sku" class="form-control mb-2" placeholder="Barcode Number" value="" />
+                                    <!--end::Input-->
+                                    <!--begin::Description-->
+                                    <div class="text-muted fs-7">Enter the product barcode number.</div>
+                                    <!--end::Description-->
+                                </div>
+                                <!--end::Input group-->
+                                <!--begin::Input group-->
+                                <div class="mb-10 fv-row">
+                                    <!--begin::Label-->
+                                    <label class="required form-label">Quantity</label>
+                                    <!--end::Label-->
+                                    <!--begin::Input-->
+                                    <div class="d-flex gap-3">
+                                        <input type="number" name="shelf" class="form-control mb-2" placeholder="On shelf" value="" />
+                                        <input type="number" name="warehouse" class="form-control mb-2" placeholder="In warehouse" />
+                                    </div>
+                                    <!--end::Input-->
+                                    <!--begin::Description-->
+                                    <div class="text-muted fs-7">Enter the product quantity.</div>
+                                    <!--end::Description-->
+                                </div>
+                                <!--end::Input group-->
+                                <!--begin::Input group-->
+                                <div class="fv-row">
+                                    <!--begin::Label-->
+                                    <label class="form-label">Allow Backorders</label>
+                                    <!--end::Label-->
+                                    <!--begin::Input-->
+                                    <div class="form-check form-check-custom form-check-solid mb-2">
+                                        <input class="form-check-input" type="checkbox" value="" />
+                                        <label class="form-check-label">Yes</label>
+                                    </div>
+                                    <!--end::Input-->
+                                    <!--begin::Description-->
+                                    <div class="text-muted fs-7">Allow customers to purchase products that are out of stock.</div>
+                                    <!--end::Description-->
+                                </div>
+                                <!--end::Input group-->
+                            </div>
+                            <!--end::Card header-->
+                        </div>
+                        <!--end::Inventory-->
+                        <!--begin::Variations-->
+                        
+                        <!--end::Variations-->
+                        <!--begin::Shipping-->
+                         
+                        <!--end::Shipping-->
+                        <!--begin::Meta options-->
+                         
+                        <!--end::Meta options-->
+                    </div>
+                </div>
+                <!--end::Tab pane-->
+            </div>
+            <!--end::Tab content-->
+            <div class="d-flex justify-content-end">
+                <!--begin::Button-->
+                <a href="../../demo7/dist/apps/ecommerce/catalog/products.html" id="kt_ecommerce_add_product_cancel" class="btn btn-light me-5">Cancel</a>
+                <!--end::Button-->
+                <!--begin::Button-->
+                <button type="submit" id="kt_ecommerce_add_product_submit" class="btn btn-primary">
+                    <span class="indicator-label">Save Changes</span>
+                    <span class="indicator-progress">Please wait...
+                    <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                </button>
+                <!--end::Button-->
             </div>
         </div>
-        <div class="d-flex justify-content-end">
-       
-            <a href="../../demo7/dist/apps/ecommerce/catalog/products.html" id="kt_ecommerce_add_product_cancel" class="btn btn-light me-5">Cancel</a>
-  
-            <button type="submit" id="kt_ecommerce_add_category_submit" class="btn btn-primary">
-                <span class="indicator-label">Save Changes</span>
-                <span class="indicator-progress">Please wait...
-                <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-            </button>
-            <!--end::Button-->
-        </div>
-    </div>
-    <!--end::Main column-->
-</form>
-
+        <!--end::Main column-->
+    </form>
+ 
+ 
     
   </div>  
 @stop

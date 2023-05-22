bbaf75af568fec5d2ecf8c92293e18af4096dc7e
Abdelrahman Magdy EL Monshed <amgo123@gmail.com>
2023-05-22 23:13:59 +0300

    fix issues

--- resources/views/backend/post_categories/create.blade.php
+++ resources/views/backend/post_categories/create.blade.php
@@ -6,91 +6,45 @@
 @stop
 
 @section('style')
-
-    @if (app()->getLocale() === 'ar')
-        <link href="{{ asset('assets/backend/plugins/custom/datatables/datatables.bundle.rtl.css') }}" rel="stylesheet"
-            type="text/css" />
-    @else
-        <link href="{{ asset('assets/backend/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet"
-            type="text/css" />
-    @endif
-
-
-
-
-
-
-
+@if (app()->getLocale() === 'ar')
+	<link href="{{ asset('assets/backend/plugins/custom/datatables/datatables.bundle.rtl.css') }}" rel="stylesheet"
+		type="text/css" />
+@else
+	<link href="{{ asset('assets/backend/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet"
+		type="text/css" />
+@endif
 @stop
 @section('content')
+
 <div class="container-xxl" id="kt_content_container">
-							 
-							<form id="kt_ecommerce_add_product_form" class="form d-flex flex-column flex-lg-row" data-kt-redirect="../../demo7/dist/apps/ecommerce/catalog/products.html">
-							 
-							
-							 
-								<div class="d-flex flex-column gap-7 gap-lg-10 w-100 mb-7 me-lg-10">
-									<!--begin:::Tabs-->
-									
-
-									
-
-<div class="card card-custom">
-	<div class="card-header ribbon ribbon-clip ribbon-right">
-		<div class="ribbon-target" style="top: 12px;">
-			<span class="ribbon-inner bg-warning"></span>Ribbon
-		</div>
-		<h3 class="card-title">
-			Clip Style
-		</h3>
-	</div>
-	<div class="card-body">
-		...
-	</div>
+		<form id="kt_ecommerce_add_product_form" class="form d-flex flex-column flex-lg-row" data-kt-redirect="../../demo7/dist/apps/ecommerce/catalog/products.html">							 
+			<div class="d-flex flex-column gap-7 gap-lg-10 w-100 mb-7 me-lg-10">
+				<!--begin:::Tabs-->									
+				<x-backend.langs.ulTabs />
+				<!--end:::Tabs-->
+				<!--begin::Tab content-->
+				<x-backend.langs.LangInputs />
+				<!--end::Tab content-->
+				<x-backend.btns.create />
+			</div>							 
+			<div class="d-flex flex-column flex-row-fluid gap-7 w-lg-400px gap-lg-10">
+				<!--begin::Thumbnail settings-->
+				<x-backend.cms.image />
+				<!--end::Thumbnail settings-->
+				<!--begin::Status-->
+				<x-backend.cms.status />
+				<!--end::Status-->
+				<!--begin::Category & tags-->
+				<x-backend.cms.category />
+				<!--end::Category & tags-->
+				<!--begin::Weekly sales-->								 
+				<x-backend.cms.tags />
+				<!--end::Weekly sales-->
+				<!--begin::Template settings-->									 
+				<!--end::Template settings-->
+			</div>
+		</form>							 
 </div>
-
-
-
-
-                                    <x-backend.langs.ulTabs />
-									<!--end:::Tabs-->
-									<!--begin::Tab content-->
-									
-                                  
-
-                                    <x-backend.langs.LangInputs />
-									<!--end::Tab content-->
-									<x-backend.btns.create />
-								</div>
-							 
-								<div class="d-flex flex-column flex-row-fluid gap-7 w-lg-400px gap-lg-10">
-									<!--begin::Thumbnail settings-->
-									
-
-									<x-backend.cms.image />
-
-									 
-
-									<!--end::Thumbnail settings-->
-									<!--begin::Status-->
-									<x-backend.cms.status />
-
-									<!--end::Status-->
-									<!--begin::Category & tags-->
-									<x-backend.cms.category />
-									<!--end::Category & tags-->
-									<!--begin::Weekly sales-->
-								 
-									<x-backend.cms.tags />
-									<!--end::Weekly sales-->
-									<!--begin::Template settings-->
-									 
-									<!--end::Template settings-->
-								</div>
-
-							</form>
-							 
-						</div>
 @stop
 
 
--- resources/views/components/backend/langs/LangInputs.blade.php
+++ resources/views/components/backend/langs/LangInputs.blade.php
@@ -1,37 +1,17 @@
-
 <div class="tab-content">
     @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
     <div class="tab-pane fade {{ LaravelLocalization::getCurrentLocaleName() == $properties['name'] ? 'show active':''}}" id="{{ substr($properties['regional'],0,2) }}" role="tabpanel">
         <div class="d-flex flex-column gap-7 gap-lg-10">
             <div class="card card-flush py-4">
-    
-                
-                <div className='rounded border p-10'>
-                    <div className="card card-bordered">
-                      <div className="card-header ribbon">
-                        <div className="card-title">Ribbon Example</div>
-                        <div className="ribbon-label bg-primary">Ribbon</div>
-                      </div>
-                      <div className="card-body">
-                        ...
-                      </div>
-                    </div>
-                  </div>
-
-                  
                 <div class="card-body pt-2">
                     <div class="mb-10 fv-row">
                         <label class="required form-label" for="title{{ substr($properties['regional'],0,2) }}">{{ __('site.title') }} [{{ substr($properties['regional'],0,2) }}]</label>
                         <input type="text" id="title{{ substr($properties['regional'],0,2) }}" name="title_{{ substr($properties['regional'],0,2) }}" class="form-control mb-2"/>
                      </div>
-
-
                     <div class="mb-10 fv-row">
                         <label class="required form-label" for="description{{ substr($properties['regional'],0,2) }}">{{ __('site.description')}} [{{ substr($properties['regional'],0,2) }}]</label>
-                            <textarea type="text" id="description_{{ substr($properties['regional'],0,2) }}" name="description_{{ substr($properties['regional'],0,2) }}" class="editor @error('description_'.substr($properties['regional'],0,2)) is-invalid @enderror"/>{{ old('description_'.substr($properties['regional'],0,2)) }}</textarea>                            
+                            <textarea rows="4" cols="30" type="text" id="description_{{ substr($properties['regional'],0,2) }}" name="description_{{ substr($properties['regional'],0,2) }}" class="editor @error('description_'.substr($properties['regional'],0,2)) is-invalid @enderror"/>{{ old('description_'.substr($properties['regional'],0,2)) }}</textarea>                            
                      </div>
-
-
                 </div>
             </div>
         </div>

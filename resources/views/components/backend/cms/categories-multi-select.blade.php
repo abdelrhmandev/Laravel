25cea4a08e644acecbf9b05b4a0d2eddee328351
Abdelrahman Magdy EL Monshed <amgo123@gmail.com>
2023-06-14 17:21:27 +0300

    add demo file

---
+++ resources/views/components/backend/cms/categories-multi-select.blade.php
@@ -0,0 +1,24 @@
+<section class="ftco-section">
+    <div class="container">
+    <div class="row justify-content-center">
+    <div class="col-md-6 text-center mb-5">
+    <h2 class="heading-section">Multiselect #01</h2>
+    </div>
+    </div>
+    <div class="row justify-content-center">
+    <div class="col-md-8 col-lg-5 d-flex justify-content-center align-items-center">
+    <div class="d-flex text-left align-items-center w-100">
+    <strong class="sl">Select Language:</strong>
+    <select id="multiple-checkboxes" multiple="multiple">
+    <option value="php">PHP</option>
+    <option value="javascript">JavaScript</option>
+    <option value="java">Java</option>
+    <option value="sql">SQL</option>
+    <option value="jquery">Jquery</option>
+    <option value=".net">.Net</option>
+    </select>
+    </div>
+    </div>
+    </div>
+    </div>
+    </section>

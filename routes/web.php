8a1210d4772ab93bc54a5840b0d3d598b4b17a91
Abdelrahman Magdy EL Monshed <amgo123@gmail.com>
2023-08-24 12:05:47 +0300

    fix issues

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,30 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('send-mail', function () {
+
+   
+
+    $details = [
+
+        'title' => 'Mail from ItSolutionStuff.com',
+
+        'body' => 'This is for testing email using smtp'
+
+    ];
+
+   
+
+    \Mail::to('your_receiver_email@gmail.com')->send(new \App\Mail\MyTestMail($details));
+
+   
+
+    dd("Email is Sent.");
+
+});
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
